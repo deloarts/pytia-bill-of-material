4558a49a2a49eb44c4a524aece4bc4e8c1b3ca26
Philip Delorenzo <git@deloarts.com>
2023-06-23 13:06:57 +0200

    Resolve path from workspace

--- pytia_bill_of_material/helper/folders.py
+++ pytia_bill_of_material/helper/folders.py
@@ -29,14 +29,14 @@ def set_default_folder_and_checkbox_from_workspace(
     """
     if workspace_folder is not None and workspace_default_path is not None:
         if os.path.isdir(workspace_default_path):
-            export_path_variable.set(str(Path(workspace_default_path)))
+            export_path_variable.set(str(Path(workspace_default_path).resolve()))
             export_checkbox_variable.set(True)
 
         elif os.path.isdir(
-            _docket_folder := Path(
+            _folder := Path(
                 workspace_folder,
                 workspace_default_path,
-            )
+            ).resolve()
         ):
-            export_path_variable.set(str(_docket_folder))
+            export_path_variable.set(str(_folder))
             export_checkbox_variable.set(True)
