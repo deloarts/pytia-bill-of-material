a0812469ff2ff6d004a1b140bb871040cd39097e
Philip Delorenzo <git@deloarts.com>
2023-11-15 14:36:16 +0100

    Version bump

--- tests/test_pytia_bill_of_material.py
+++ tests/test_pytia_bill_of_material.py
@@ -4,7 +4,7 @@ import toml
 
 from pytia_bill_of_material.const import APP_VERSION
 
-VERSION = "0.9.1"
+VERSION = "0.9.2"
 
 directory = os.path.dirname(os.path.realpath("__file__"))
 with open(os.path.join(directory, "pyproject.toml"), "r") as f:
